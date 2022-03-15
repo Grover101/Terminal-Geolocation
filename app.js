b2afe2b9f74f3af2b86a101ceb3c34534549d612
Grover101 <limachigrover09@gmail.com>
2022-03-14 22:31:40 -0400

    add: clase busquedas y tareas a realizar en opcion 1

--- app.js
+++ app.js
@@ -1,11 +1,43 @@
-const { listMenu, pause } = require('./helpers/inquirer')
+const Searches = require('./models/searches')
+const { listMenu, pause, readInput } = require('./helpers/inquirer')
 
 const main = async () => {
+    const search = new Searches()
     let option
+
     do {
         option = await listMenu()
-        console.log(option)
-        await pause()
+
+        switch (option) {
+            case 1:
+                // TODO: Mostrar mensaje
+                const place = await readInput('Enter a City:')
+                console.log(place)
+
+                // TODO: Buscar los lugares
+
+                // TODO: Seleccionar el lugar
+
+                // TODO: Clima
+
+                // TODO: Mostrar resultados
+                console.log('\nCity Infomatica\n'.green)
+                console.log('City:')
+                console.log('Lat:')
+                console.log('Lng:')
+                console.log('Temperature:')
+                console.log('Min Temperature:')
+                console.log('Max Temperature:')
+                break
+            case 2:
+                // TODO: Mostrar historial
+                break
+            case 0:
+                console.log('Bye!')
+                break
+        }
+
+        if (option !== 0) await pause()
     } while (option !== 0)
 }
 
---
+++ models/searches.js
@@ -0,0 +1,12 @@
+class Searches {
+    record = ['Madrid', 'Mexico', 'Barcelona']
+
+    constructor() {}
+
+    async city(place = '') {
+        console.log(place)
+        return []
+    }
+}
+
+module.exports = Searches
